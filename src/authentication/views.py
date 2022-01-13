36af9d30468f93e66cc49d5f1f41d2087713a4fb
Cedric Morgantini <c.morgantini@free.fr>
2022-01-13 20:25:42 +0100

    modification admin.py

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,13 +1,15 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from authentication.serializers import UsersSerializer
-
+from crm.permissions import IsUserIsManagementTeam
 # Create your views here.
 
 
 @api_view(['POST', ])
+@permission_classes([IsAuthenticated, IsUserIsManagementTeam])
 def signup_view(request):
 
     if request.method == 'POST':
