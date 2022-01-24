78efb4d4952bd6c594d22862f6c3f3d48fbf0c8e
Cedric Morgantini <c.morgantini@free.fr>
2022-01-24 15:37:59 +0100

    Corrections dans scripts authentication

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,23 +1,7 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
-
-
-# class Users(AbstractUser):
-#     """Stocke les identifiants de connexion des utilisateurs"""
-#     first_name = models.CharField(max_length=15, verbose_name="prenom")
-#     last_name = models.CharField(max_length=15, verbose_name="nom")
-#     email = models.EmailField(unique=True, max_length=255)
-#     is_sales = models.BooleanField('sales team', default=False)
-#     is_support = models.BooleanField('support team', default=False)
-#     is_management = models.BooleanField('management team', default=False)
-#     # username = None
-#
-#     # choix de l'email et nom du prénom, car plusieurs prénoms identiques possibles
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name']rManager
 
 class MyUserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
@@ -59,3 +43,6 @@ class Users(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+    class Meta:
+        verbose_name = "Utilisateur"
